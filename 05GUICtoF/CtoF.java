745d6dfe8462bb4b314a95294f1f9f612d9f56b4
Nino Dickersin <n.dickersin@gmail.com>
2014-12-11 00:05:49 -0500

    Celsius to Fahrenheit converter

--- 05GUICtoF/CtoF.java
+++ 05GUICtoF/CtoF.java
@@ -1,13 +1,15 @@
+import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 public class CtoF extends JFrame implements ActionListener{
     private Container pane;
-
+    private Container buttons;
+    private Container textyStuff;
     private JButton b;
     private JLabel l;
-    private JTextField t;
-    private JCheckBox c;
-    public Window2() {
+    private JTextField text;
+    private JCheckBox box;
+    public void CtoF() {
 	this.setTitle("Celsius-Fahrenheit Converter");
 	this.setSize(600,400);
 	this.setLocation(100,100);
@@ -15,29 +17,46 @@ public class CtoF extends JFrame implements ActionListener{
 
 	
 	pane = this.getContentPane();
-	pane.setLayout(new FlowLayout());
+	pane.setLayout(new GridLayout(2,1));
 
-	l = new JButton("Convert");
-	b = new JLabel("",null,JLabel.CENTER);
-	t = new JTextField(12);
-	c = new JCheckBox("Overclock!!!");
-	pane.add(l);
-	pane.add(b);
-	pane.add(t);
-	pane.add(c);
+	b = new JButton("Convert");
+	l = new JLabel("Number",null,JLabel.CENTER);
+	text = new JTextField(40);
+	box = new JCheckBox("Fahrenheit to Celsius");
+
+	b.setActionCommand("convert");
+	b.addActionListener(this);
+
+	buttons = new Container();
+	buttons.setLayout(new FlowLayout());
+	buttons.add(b);
+	buttons.add(box);
+
+	textyStuff = new Container();
+	textyStuff.setLayout(new FlowLayout());
+	textyStuff.add(l);
+	textyStuff.add(text);
+
+	pane.add(textyStuff);
+	pane.add(buttons);
     }
     public void actionPerformed(ActionEvent e){
-	if("Convert".equals(e.getActionCommand()){
-		String s = t.getText();
-		int i = Integer.parseInt(s);
+	String action = e.getActionCommand();
+	if(action.equals("convert")){
+	    String s = text.getText();
+	    int i = Integer.parseInt(s);
+	    if(box.isSelected()){
+		i = (i - 32) * (5/9);
+	    }else{
 		i = i*(9/5) + 32;
+	    }
+	    s = ""+i;
+	    text.setText(s);
 	}
     }
-    l.addActionListener(this);
-    l.setActionCommand();
 
     public static void main(String[] args) {
-        Window2 g = new Window2();
+        CtoF g = new CtoF();
         g.setVisible(true);
     }
 }
