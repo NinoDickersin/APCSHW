4ace83917a5bebc7cf866bfba51e923a82406be4
Nino Dickersin <n.dickersin@gmail.com>
2014-12-17 13:11:55 -0500

    bubble() works, although sorts backwards; added selection and insertion sorts

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -11,19 +11,45 @@ public class Sorts{
 	    }
 	} 
     }
-
-    public String toString(){
-	String s = "[ ";
-	for (int i = 0; i < this.length; i ++){
-	    s += this[i] + " ";
+    public static void selectionSort(int[]c){
+	int d, j;
+	int smallest;
+	int temp;
+	for(int i = 0; i < c.length;i++){
+	    smallest = c[i];
+	    d = i + 1;
+	    j = i;
+	    for(;d < (c.length - 1);d++){
+		if (c[d] < smallest){
+		    smallest = c[d];
+		    j = d;
+		}
+	    }
+	    temp = c[i];
+	    c[i] = smallest;
+	    c[j] = temp;
 	}
-	s += "]";
-	return s;
+    }
+
+    public static void insertionSort(int[]c){
+	int e = 0;
+	for(int i = 1; i < c.length; i ++){
+	    if(c.length > 1 && c[i] > c[i + 1]){
+		e = c[i];
+		int j = i;
+		for (;j >= 1 && e > c[j + 1]; j --){
+		    c[j] = c[j - 1];
+		}
+		c[j] = e;
+	    }
+	} 
     }
 
     public static void main(String[]args){
 	int[] a = new int [10];
-	System.out.println(a);
+	for(int i = 0; i < a.length; i ++){
+	    System.out.println(a[i]);
+	}
 	a[0] = 7;
 	a[1] = 88;
 	a[2] = 29;
@@ -35,6 +61,8 @@ public class Sorts{
 	a[8] = 2;
 	a[9] = 10;
 	bubble(a);
-	System.out.println(a);
+	for(int i = 0; i < a.length; i ++){
+	    System.out.println(a[i]);
+	}
     }
 }
